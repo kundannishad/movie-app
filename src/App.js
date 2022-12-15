e682904e577a353104f05422e8a864bea160d6e3
kundanpbpartners <kundan.kumar@pbpartners.com>
2022-12-15 15:56:57 +0530

    kc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import './App.scss';
+import React from 'react';
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-        <h>Movie</h>
+     <BrowserRouter>
+          <Header/>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/movie/:imdbID" element={<MovieDetail />}></Route>
+          <Route path="/*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey='1d393edb';
